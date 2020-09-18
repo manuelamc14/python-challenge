0c8e3d352223f5b99a46bf8730d2ad76d00d9120
Manuela Munoz <manuelamunoz103@gmail.com>
2020-09-17 20:42:34 -0400

    Average of the changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,7 +4,7 @@ import csv
 
 # Set path for file
 
-csv_path = os.path.join("Resources", "budget_data.csv")
+csv_path = os.path.join("..","Resources", "budget_data.csv")
 
 # Open the csv
 
@@ -26,12 +26,26 @@ with open(csv_path) as csv_file:
     for row in csv_reader:
         # total number of months
         total_months += 1
-          
+         
         # Add monthly profits or loses
 
         pl_list.append(int(row[1]))
-
-    #net total amount of "Profit/Losses"
+         
+    # net total amount of "Profit/Losses"
     total_pl = sum(pl_list)
+    print(total_pl)
+    print(total_months) 
     
+    # Create a list to store the variance month by month
+
+    variance = [y-x for x, y in zip(pl_list, pl_list[1:])]
+
+    # Average of the changes in "Profit/Losses" over the entire period
+
+    variance_average = round(sum(variance) / len(variance), 2)
+    print(variance)
+    print(variance_average)
+
+    # Greatest Increase in Profits
+
  
