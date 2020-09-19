328a570ab498186512b91243cd6eb1c06b96ece7
Manuela Munoz <manuelamunoz103@gmail.com>
2020-09-19 18:01:42 -0400

    A complete list of candidates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,11 @@
 # Modules
 import os
 import csv
+import statistics
 
 # Set path for file
 
-csv_path = os.path.join("..","Resources", "election_data.csv")
+csv_path = os.path.join("Resources", "election_data.csv")
 
 # Open csv
 
@@ -18,9 +19,18 @@ with open(csv_path) as csv_file:
     # Starting variables
 
     total_votes = 0
-
-    # Total of number of votes
+    candidates = []
 
     for row in py_poll:
+        # Total votes
+
         total_votes += 1
-    print("Total Votes: {}".format(total_votes))
+ 
+        # For readability, we store the data in one variable
+
+        candidates.append(row[2])
+
+    # A complete list of candidates who received votes
+    
+    candidate_list = list(set(candidates))
+
